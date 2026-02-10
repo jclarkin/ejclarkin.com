9ddccb65e886055c2181631817464568e094ef22
Jonathan Clarkin <j.r.clarkin@gmail.com>
2026-02-10 16:57:43 -0500

    Add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ejclarkin.com',
-  output: 'static'
+  output: 'static',
+  integrations: [
+    sitemap()
+  ]
 });
